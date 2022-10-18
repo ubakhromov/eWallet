31c461ad8b65218259d06e1e6e3de51c45b432fd
Ulugbek <ubakhromov22@gmail.com>
2022-10-18 14:45:23 +0500

    INFRA: Initialize API Project

---
+++ eWallet.Api/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RESTFulSense.Controllers;
+
+namespace eWallet.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : RESTFulController
+    {
+        private readonly ILogger<HomeController> logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            logger.LogTrace("I am logging trace");
+            logger.LogInformation("I am logging info");
+            logger.LogWarning("I am logging warning");
+            logger.LogError("I am logging error");
+            logger.LogCritical("I am logging critical");
+
+            return Ok("Here we go!");
+        }
+    }
+}
